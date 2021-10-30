c187d64c5fbd79ec22863f6aadff4183cb25c538
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-30 09:42:59 +0300

    HOW TO: instantiating a class

---
+++ OOP_Fundamentals/class_instance.php
@@ -0,0 +1,26 @@
+<?php
+
+/* Defining a class
+*/
+
+class Cars {
+    // Here are the properties (usually variables)
+
+    // Here are the methods (functions)
+    function gretting(){
+        echo "Hello World";
+    }
+
+}
+
+/* Instanciating
+*/
+$bmw = new Cars();
+$toyota = new Cars();
+
+/* Activating method greeting from Cars class
+*/
+$bmw->gretting();
+
+
+?>
