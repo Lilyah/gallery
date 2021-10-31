0bf465d0ba345cc7f87e5c98c09d2820a5cf2438
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-10-31 10:34:08 +0200

    changing .php and .html paths to be without extentions

--- admin/includes/side_nav.php
+++ admin/includes/side_nav.php
@@ -1,16 +1,16 @@
 <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li>
-                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="index"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="users.php"><i class="glyphicon glyphicon-user"></i> Users</a>
+                        <a href="users"><i class="glyphicon glyphicon-user"></i> Users</a>
                     </li>
                     <li>
-                        <a href="upload.php"><i class="glyphicon glyphicon-upload"></i> Upload</a>
+                        <a href="upload"><i class="glyphicon glyphicon-upload"></i> Upload</a>
                     </li>
                     <li>
-                        <a href="comments.php"><i class="glyphicon glyphicon-comment"></i> Comments</a>
+                        <a href="comments"><i class="glyphicon glyphicon-comment"></i> Comments</a>
                     </li>
                 </ul>
             </div>
