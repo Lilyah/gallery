c2e1e0b09aaa3e8dc66b3e38cac1151d8d9be337
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-01 13:53:40 +0200

    removing

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -11,8 +11,4 @@ defined("DB_USER") ? null : define("DB_USER", "root");
 defined("DB_PASSWORD") ? null : define("DB_PASSWORD", "");
 defined("DB_NAME") ? null : define("DB_NAME", "gallery_db");
 
-/* Connect to the DB
-*/
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
 ?>
