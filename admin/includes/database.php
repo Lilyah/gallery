9344624e01ab2883144972982520304b75e775db
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-01 16:34:27 +0200

    changing code to OOP style

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -17,33 +17,44 @@ class Database {
 
     // Connect to the DB
     public function open_db_connection(){
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        // Creating new mysqli object
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-        if(mysqli_connect_errno()){
-            die("Database connection failed badly" . mysqli_error($this->connection));
+        if($this->connection->connect_errno){
+            // connect_error and connect_errno are build-in
+            die("Database connection failed badly" . $this->connection->connect_error);
         }
     }
 
     // Quqery builder
     public function query($sql){
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
+        // Using custom method confirm_query
+        $this->confirm_query($result);
         return $result;
     }
 
-    // Confirm queri
+    // Confirm query
     private function confirm_query($result){
         if(!$result){
-            die("Query Failed");
+            die("Query Failed" . $this->connection->error);
         }
     }
 
     // Escape strings when input data
     public function escape_string($string){
-        $escape_string = mysqli_real_escape_string($this->connection, $string);
+        $escape_string = $this->connection->real_escape_string($string);
         return $escape_string;
     }
 
+    //
+    public function the_insert_id(){
+        return $this->connection->insert_id;
+    }
+
 }
+
+
 
 $database = new Database;
 
