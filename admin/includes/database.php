6f3f98644c715579c82705399b5291b77e4decd9
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-01 14:08:05 +0200

    developing Query builder, Confirm query, Escape string

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -15,16 +15,34 @@ class Database {
         $this->open_db_connection();
     }
 
+    // Connect to the DB
     public function open_db_connection(){
-        // Connect to the DB
         $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
         if(mysqli_connect_errno()){
             die("Database connection failed badly" . mysqli_error($this->connection));
         }
-
-
     }
+
+    // Quqery builder
+    public function query($sql){
+        $result = mysqli_query($this->connection, $sql);
+        return $result;
+    }
+
+    // Confirm queri
+    private function confirm_query($result){
+        if(!$result){
+            die("Query Failed");
+        }
+    }
+
+    // Escape strings when input data
+    public function escape_string($string){
+        $escape_string = mysqli_real_escape_string($this->connection, $string);
+        return $escape_string;
+    }
+
 }
 
 $database = new Database;
