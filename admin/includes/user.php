559838abcd1afa9afdb5562c93643ebd540a4570
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-02 12:53:00 +0200

    creating method find_user_by_id()

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -9,11 +9,21 @@ class User {
     function __construct(){
     }
 
+    // Finding all users
     static function find_all_users(){
         global $database;
 
         $result_set = $database->query("SELECT * FROM users");
         return $result_set;
+    }
+
+    // Finding user by id
+    static function find_users_by_id($user_id){
+        global $database;
+
+        $result_set = $database->query("SELECT * FROM users WHERE user_id = $user_id");
+        $found_user = mysqli_fetch_array($result_set);
+        return $found_user;
     }
 
 
