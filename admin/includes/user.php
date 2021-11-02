f2537c1a83e19006ff8b97269a5bcf0fa7c00d46
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-02 16:00:42 +0200

    comments how the methods work with each other

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -19,14 +19,37 @@ class User {
     }
 
     // Finding all users
+    /* User::find_all()  ... Here is the flow once is called 
+        1.   It goes to the find_all method 
+        2. The find_all() returns the find_by_query() results 
+        3. The find_by_query()  does a couple things 
+            A. it makes the query 
+            B. Fetches the the data from database table using  a while loop and it returns it in $row
+            C. Passes the results ($row) to the Instantiation (instantantion - weird name I know) method
+            D. Returns the object in the $the_object_array variable that it gets from the  instantantion method.
+            E. And that will be the result that find_all() returns when we use User::find_all()
+    */
     public static function find_all_users(){
         return User::find_this_query("SELECT * FROM users");
     }
 
     // Finding user by id
     public static function find_users_by_id($user_id){
-        $result_set = User::find_this_query("SELECT * FROM users WHERE user_id = $user_id");
-        $found_user = mysqli_fetch_array($result_set);
+        $the_result_array = User::find_this_query("SELECT * FROM users WHERE user_id = $user_id");
+
+        /* ONE way of doing this  
+        */
+        // if(!empty($the_result_array)){
+        //     $first_item = array_shift($the_result_array);
+        //     return $first_item;
+        // } else {
+        //     return false;
+        // }
+
+        /* SECOND way of doing this is Ternary Behaviour
+        */
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
         return $found_user;
     }
 
@@ -45,9 +68,17 @@ class User {
         return $the_object_array;
     }
 
-    // Method for automathic instatiation of class User and hiw properties
+    // Method for automathic instatiation of class User and his properties
+    /* What the Instantation method is doing
+        1. Gets the calling class name.
+        2. Creates an instance of the class
+        3. It loops through the $the_record variable that has all the records
+        4. It checks to see if the properties exist on that object with the other method has_the_property() 
+        5. If the keys from the record which basically are the columns from db table are found or equal the object properties then it assigns the values to them.
+        6. Finally it returns the object!
+    */
     public static function instantiation($found_user){
-        $user = new User;
+        $user = new User; 
 
         /* The foreach code below is actually doing this here
         */
