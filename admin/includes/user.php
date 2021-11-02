4cc896fa93f8b2b9483bc7c7635dc414fa1e7e52
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-02 13:19:50 +0200

    improving the methods code

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -11,21 +11,23 @@ class User {
 
     // Finding all users
     static function find_all_users(){
-        global $database;
-
-        $result_set = $database->query("SELECT * FROM users");
-        return $result_set;
+        return User::find_this_query("SELECT * FROM users");
     }
 
     // Finding user by id
     static function find_users_by_id($user_id){
-        global $database;
-
-        $result_set = $database->query("SELECT * FROM users WHERE user_id = $user_id");
+        $result_set = User::find_this_query("SELECT * FROM users WHERE user_id = $user_id");
         $found_user = mysqli_fetch_array($result_set);
         return $found_user;
     }
 
+    // Method for executing any query
+    public static function find_this_query($sql){
+        global $database;
+
+        $result_set = $database->query($sql);
+        return $result_set;
+    }
 
 
 
