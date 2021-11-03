d2633d153579d72316746837a9069476004cc64f
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-03 11:44:59 +0200

    including classAutoLoader

---
+++ admin/includes/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// Load files with classes if they are not included in init.php
+function classAutoLoader($class){
+    $class = strtolower($class);
+    $the_path = 'includes/'.$class.'.php';
+
+    if(file_exists($the_path)){
+        require_once($the_path);
+    } else {
+        die("The file {$class}.php was not found.");
+    }
+};
+
+spl_autoload_register('classAutoLoader')
+
+?>
--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,5 +1,9 @@
 <?php
 
+// In functions.php has claaAutoLoader in case you forget to include some class here
+require("functions.php");
+
+
 include("config.php");
 require("database.php");
 require("user.php");
