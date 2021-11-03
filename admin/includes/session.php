882856dddf486fa4d901c619eb3b0df2ca1e386f
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-03 12:17:39 +0200

    developig method check_the_login()

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -16,6 +16,17 @@ class Session {
         session_start();
     }
 
+    // Check if the session user_id is set
+    private function check_the_login(){
+        if(isset($_SESSION['user_id'])){
+            $this->user_id = $_SESSION['user_id']; // Assigning value to our object
+            $this->signed_in = true;
+        } else {
+            unset($this->user_id);
+            $this->signed_in = false;
+        }
+    }
+
 
 }
 
