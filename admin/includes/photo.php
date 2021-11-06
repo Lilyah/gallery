f0a90fe6bc65ee770a008644f075d0ca71b24d43
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-06 18:42:44 +0200

    fixing values of db_table name and db_table_fields array

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -6,9 +6,10 @@ class Photo extends Db_object {
 
     /* Properties
     */
-    protected static $db_table = "photo"; // Makes the method below more flexible, because we can use them with different tables
-    protected static $db_table_fields = array('photo_id', 'photo_title', 'photo_description', 'photo_filename', 'photo_type', 'photo_size');
+    protected static $db_table = "photos"; // Makes the method below more flexible, because we can use them with different tables
+    protected static $db_table_fields = array('photo_title', 'photo_description', 'photo_filename', 'photo_type', 'photo_size');
     public $photo_id;
+    public $photo_title;
     public $photo_description;
     public $photo_filename;
     public $photo_type;
