ae7d041f8d6414f80fbac8c1fd24a2cc1f4377f9
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-06 17:51:05 +0200

    setting properties objects

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -6,7 +6,13 @@ class Photo extends Db_object {
 
     /* Properties
     */
-
+    protected static $db_table = "photo"; // Makes the method below more flexible, because we can use them with different tables
+    protected static $db_table_fields = array('photo_id', 'photo_title', 'photo_description', 'photo_filename', 'photo_type', 'photo_size');
+    public $photo_id;
+    public $photo_description;
+    public $photo_filename;
+    public $photo_type;
+    public $photo_size;
 
 
     /* Methods
