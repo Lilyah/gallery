1fb7244c71e9b22d131d7548815af0f470291148
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-12 15:20:41 +0200

    calling delete_photo() method

---
+++ admin/delete_photo.php
@@ -0,0 +1,26 @@
+<?php 
+
+include("includes/init.php"); 
+
+if(!$session->is_signed_in()){
+     redirect("login.php");
+}
+
+// If for some reason the GET request is empty will redirect back to index
+if(empty($_GET['id'])){
+    redirect("../photos");
+}
+
+
+$photo = Photo::find_by_id($_GET['id']); // Finding the photo by id
+
+if($photo){ // If we have the photo then we will delete it
+    $photo->delete_photo();
+    redirect("../photos");
+} else {
+    redirect("../photos"); // Id we haven't the photo it will redirect to photos.php
+}
+
+?>
+
+        
