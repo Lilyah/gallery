a6c82b012cb7e40910ec57e26056ccaaac297a9b
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-15 19:17:19 +0200

    added Add User btn

--- admin/users.php
+++ admin/users.php
@@ -37,6 +37,8 @@ $users = User::find_all(); // Instantiating class Photo
                             <small>Subheading</small>
                         </h1>
 
+                        <a href="add_user.php" class="btn btn-primary">Add User</a>
+
                         <div class="col-md-12">
                             <table class="table table-hover">
                                 <thead>
