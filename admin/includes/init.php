e0a92f7713ce0861ce477be8d96f66fe835c0269
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-16 13:03:03 +0200

    changing SITE_ROOT definition; moving up require session file

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -2,7 +2,8 @@
 
 // Directory paths
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR); // \ or / depending on what system you are
-defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'Applications' . DS . 'XAMPP' . DS . 'xamppfiles' . DS . 'htdocs' . DS . 'gallery'); // \ or / depending on what system you are
+                            
+defined('SITE_ROOT') ? null : define('SITE_ROOT', __DIR__ . DS . '..' . DS . '..'); // \ or / depending on what system you are
 
 defined("INCLUDES_PATH") ? null : define("INCLUDES_PATH", SITE_ROOT . DS . 'admin' . DS . 'includes');
 
@@ -11,10 +12,10 @@ require("functions.php");
 
 // ! Files that contain configuration or let's say objects or variables that other files depend on, must be loaded first.
 include("config.php");
+require("session.php");
 require("database.php");
 require("db_object.php");
 require("user.php");
 require("photo.php");
-require("session.php");
 
 ?>
