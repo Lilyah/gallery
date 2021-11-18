5fb83fd1df30d7b324c63d22733e951638705522
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-18 12:50:21 +0200

    changing paths for required files

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -8,14 +8,15 @@ defined('SITE_ROOT') ? null : define('SITE_ROOT', __DIR__ . DS . '..' . DS . '..
 defined("INCLUDES_PATH") ? null : define("INCLUDES_PATH", SITE_ROOT . DS . 'admin' . DS . 'includes');
 
 // In functions.php has claaAutoLoader in case you forget to include some class here
-require("functions.php");
+require_once(INCLUDES_PATH.DS."functions.php");
 
 // ! Files that contain configuration or let's say objects or variables that other files depend on, must be loaded first.
-include("config.php");
-require("session.php");
-require("database.php");
-require("db_object.php");
-require("user.php");
-require("photo.php");
+require_once(INCLUDES_PATH.DS."config.php");
+require_once(INCLUDES_PATH.DS."session.php");
+require_once(INCLUDES_PATH.DS."database.php");
+require_once(INCLUDES_PATH.DS."db_object.php");
+require_once(INCLUDES_PATH.DS."user.php");
+require_once(__DIR__ . DS . "photo.php");
+require_once(INCLUDES_PATH.DS."comment.php");
 
 ?>
