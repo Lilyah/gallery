b97f0219d8b536dac2150dfa4c9aa911eee8583f
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-18 15:13:50 +0200

    implementing methods from class Comment and displaying the comments on front-end successfully

--- photo.php
+++ photo.php
@@ -9,11 +9,23 @@ if(empty($_GET['id'])){
 
 $photo = Photo::find_by_id($_GET['id']);
 
-echo $photo->photo_title;
-
 if(isset($_POST['submit'])){
-    echo "It works";
+    $author = trim($_POST['author']);
+    $body   = trim($_POST['body']);
+
+    $new_comment = Comment::create_comment($photo->id, $author, $body);
+
+    if($new_comment && $new_comment->save()) {
+        redirect("photo.php?id={$photo->id}");
+    } else {
+        $message = "There was some problems saving your comment";
+    }
+} else {
+    $author = "";
+    $body = "";
 }
+
+$comments = Comment::find_the_comments($photo->id);
 
 
 ?>
@@ -123,7 +135,6 @@ if(isset($_POST['submit'])){
                 <!-- Blog Comments -->
 
 
-
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
@@ -143,18 +154,22 @@ if(isset($_POST['submit'])){
 
                 <!-- Posted Comments -->
 
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                <?php foreach ($comments as $comment) : ?>
+                    <!-- Comment -->
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading"><?php echo $comment->author; ?>
+
+                                <small>August 25, 2014 at 9:30 PM</small>
+                            </h4>
+                            <?php echo $comment->body; ?>
+                        </div>
                     </div>
-                </div>
+                <?php endforeach;?>
+
 
             </div>
 
