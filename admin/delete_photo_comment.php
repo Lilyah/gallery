99fba40aead7f594d1ff2e12182a0003b580d36c
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-19 12:49:27 +0200

    functionality for deleting comment on specific photo

---
+++ admin/delete_photo_comment.php
@@ -0,0 +1,25 @@
+<?php 
+
+include("includes/init.php"); 
+
+if(!$session->is_signed_in()){
+     redirect("login.php");
+}
+
+// If for some reason the GET request is empty will redirect back to index
+if(empty($_GET['id'])){
+    redirect("comments");
+}
+
+$comment = Comment::find_by_id($_GET['id']); // Finding the user by id
+
+if($comment){ // If we have the comment then we will delete it
+    $comment->delete();
+    redirect("photo_comment.php?id={$comment->photo_id}");
+} else {
+    redirect("photo_comment.php?id={$comment->photo_id}"); // If we haven't the comment it will redirect to comments.php
+}
+
+?>
+
+        
