ce18005b7848624ca56d67576e275a21ca16b5f4
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-22 13:04:31 +0200

    personalizing Google Pie Chart data

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -20,14 +20,15 @@
 
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
+          ['Views ', <?php echo $session->count; ?>],
+          ['Comments', <?php echo Comment::count_all(); ?>],
+          ['Users', <?php echo User::count_all(); ?>],
+          ['Photos', <?php echo Photo::count_all(); ?>]
         ]);
 
         var options = {
+          backgroundColor: '#0000ffff',
+          pieSliceText: 'percentage', 
           title: 'My Daily Activities'
         };
 
