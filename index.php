caba05996807c29547dd3d9817cce82ce6a0e061
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-22 13:36:48 +0200

    developing basic html for showing the photos; it has bugs

--- index.php
+++ index.php
@@ -2,17 +2,32 @@
 
 include("includes/header.php"); 
 
-$photos = Photo::find_all(); ?>
+$photos = Photo::find_all(); 
+
+?>
 
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
+            <div class="col-md-12">
 
-    
-            
-          
+                    <div class="thumbnails row">
+
+                        <?php foreach ($photos as $photo) : ?>
+
+                        <div class="col-xs-6 col-md-3">
+                            <a href="" class="thumbnail">
+                                <img class="" src="admin/<?php echo $photo->picture_path(); ?>">           
+                            </a>                 
+                        </div>
+
+                        <?php endforeach;?>
+
+                    </div>
+
+
+
          
 
             </div>
