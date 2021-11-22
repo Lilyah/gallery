072be9d9144657f77ce377655e32038bde832b89
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-22 12:15:34 +0200

    implementing count_all() for counting photos in the system

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -49,7 +49,7 @@
                                            </div>
 
                                            <div class="col-xs-9 text-right">
-                                               <div class="huge">7</div>
+                                               <div class="huge"><?php echo Photo::count_all(); ?></div>
                                                <div>Photos</div>
                                            </div>
 
