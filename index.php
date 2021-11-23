25541b5c13ce7616b023de671ca185f58d20d771
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-23 16:54:41 +0200

    developing for loop for paginations numbers

--- index.php
+++ index.php
@@ -49,12 +49,24 @@ $photos = Photo::find_by_query($sql); // executing the query
                                 // If we have more than 1 page then will be displayed Next and Previous btns
                                 if($paginate->page_total() > 1){
 
-                                    // If we have next page then weill be displayed the Next btn
+                                    // If we have next page then will be displayed the Next btn
                                     if($paginate->has_next()){
                                         echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>"; // Classes and hrefs are with single quotes because we are using them with php
                                     }
 
-                                    // If we have previous page then weill be displayed the Previous btn
+                                    // Loop for displaying the numbers of the pages
+                                    for ($i=1; $i <= $paginate->page_total() ; $i++){
+
+                                        if($i == $paginate->current_page){
+                                            echo "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+                                        } else {
+                                            echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                                        }
+
+                                    }
+                   
+
+                                    // If we have previous page then will be displayed the Previous btn
                                     if($paginate->has_previous()){
                                         echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>"; // Classes and hrefs are with single quotes because we are using them with php
                                     }
