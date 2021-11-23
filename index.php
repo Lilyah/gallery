c65c6a2d6f7f56968eae4ec3340c5a816ba0a53a
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-23 15:58:04 +0200

    instantiating class Paginate and build a query

--- index.php
+++ index.php
@@ -6,7 +6,17 @@ $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
 $items_per_page = 4;
 $items_total_count = Photo::count_all();
 
-$photos = Photo::find_all(); 
+//$photos = Photo::find_all(); 
+
+// Instantiating class Paginate
+$paginate = new Paginate($page, $items_per_page, $items_total_count);
+
+// Query
+$sql = "SELECT * FROM photos ";
+$sql .= "LIMIT {$items_per_page} ";
+$sql .= "OFFSET {$paginate->offset()}";
+$photos = Photo::find_by_query($sql); // executing the query
+
 
 ?>
 
