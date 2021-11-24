c3669fe90d25d54ebc60fad53cde805603a5afed
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-24 16:51:16 +0200

    adding id to the Delete btn in order to pull out the user id with javascript

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -93,7 +93,7 @@ if(empty($_GET['id'])){
                                 </div>
 
                                 <div class="form-group">
-                                    <a class="btn btn-danger pull-left" href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
+                                    <a id="user-id" class="btn btn-danger pull-left" href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
                                     <input type="submit" name="update" class="btn btn-primary pull-right" value="Update">
                                 </div>
 
