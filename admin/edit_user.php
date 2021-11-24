1b81beb81bc288d39abddafe773e89eefaffb7a3
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-24 13:37:08 +0200

    adding basic modal html and css code

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -30,6 +30,66 @@ if(empty($_GET['id'])){
 
 ?>
 
+
+<!-- Modal START -->
+
+<div class="modal fade" id="photo-library">
+
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Gallery System Library</h4>
+            </div>
+
+            <div class="modal-body">
+
+                <div class="col-md-9">
+                    <div class="thumbnails row">
+
+                        <!-- PHP LOOP HERE CODE HERE-->
+
+                        <div class="col-xs-2">
+
+                            <a role="checkbox" aria-checked="false" tabindex="0" id="" href="#" class="thumbnail">
+                            <img class="modal_thumbnails img-responsive" src="<!-- PHP LOOP HERE CODE HERE-->" data="<!-- PHP LOOP HERE CODE    HERE-->">
+                            </a>
+
+                            <div class="photo-id hidden"></div>
+
+                        </div>
+
+                        <!-- PHP LOOP HERE CODE HERE-->
+
+                    </div>
+                </div><!--col-md-9 -->
+
+                <div class="col-md-3">
+                    <div id="modal_sidebar"></div>
+                </div>
+
+            </div><!--Modal Body-->
+
+            <div class="modal-footer">
+                <div class="row">
+                    <!--Closes Modal-->
+                    <button id="set_user_image" type="button" class="btn btn-primary" disabled="true" data-dismiss="modal">Apply Selection</button>
+                </div>
+            </div>
+
+        </div><!-- /.modal-content -->
+
+    </div><!-- /.modal-dialog -->
+    
+</div><!-- /.modal -->
+
+
+<!-- Modal END -->
+
+
+
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         
@@ -61,7 +121,7 @@ if(empty($_GET['id'])){
                             <div class="col-md-6 col-md-offset-3">
 
                                 <div class="form-group">
-                                    <a class="thumbnail" href="#">
+                                    <a class="thumbnail" href="#" data-toggle="modal" data-target="#photo-library">
                                         <img src="<?php echo $user->photo_path_placeholder(); ?>">
                                     </a>
                                 </div>
