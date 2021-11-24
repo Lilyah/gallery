74fdc98ca93d85e47c19e51f7295d1477decc0a9
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-24 16:52:02 +0200

    pulling out the user_id for modal_thumbnails

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
     $('#summernote').summernote({
       height: 300
     });
+
+
+
+    var user_href;
+    var user_href_splitted;
+    var user_id;
+
+    $(".modal_thumbnails").click(function(){
+      $("#set_user_image").prop('disabled', false);
+      user_href = $("#user-id").prop('href');
+      user_href_splitted = user_href.split("=");
+      user_id = user_href_splitted[user_href_splitted.length - 1];
+    });
+
+
 });
