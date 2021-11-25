77dee63c9c67576512f7600df38257cbd541c460
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-11-25 12:06:00 +0200

    creating php code for ajax and modal

---
+++ admin/includes/ajax_code.php
@@ -0,0 +1,11 @@
+<?php
+
+require("init.php");
+
+$user = new User();
+
+if(isset($_POST['image_name'])){
+    $user = $user->ajax_save_user_image($_POST['image_name'], $_POST['user_id']); // this is coming from class User
+}
+
+?>
